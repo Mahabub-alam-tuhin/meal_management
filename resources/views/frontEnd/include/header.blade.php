fc3f69ea931b1e2c10763c57261385b3445786c6
Md-shefat-masum <mshefat924@gmail.com>
2023-11-23 18:19:18 +0600

    update

--- resources/views/frontEnd/include/header.blade.php
+++ resources/views/frontEnd/include/header.blade.php
@@ -1,18 +1,15 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between demo_container">
-
-      <a href="{{ route('/') }}" class="logo d-flex align-items-center me-auto me-lg-0 demo_logo">
-
-        <h1 class="meal">Meal Management<span>.</span></h1>
-      </a>
-
-     <!-- .navbar -->
-     <div class="user">
-     <a class="btn-book-a-table register" href="{{ route('frontEnd.register.register') }}">Register</a>
-      <a class="btn-book-a-table log" href="{{ route('frontEnd.login.login') }}">Login</a>
-     </div>
-      <i class="mobile-nav-toggle mobile-nav-show bi bi-list cross"></i>
-      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <a href="{{ route('/') }}" class="logo d-flex align-items-center me-auto me-lg-0 demo_logo">
+                    <h5 class="meal">Meal Management<span>.</span></h5>
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 text-end">
+                <a class="btn-book-a-table btn-sm register" href="{{ route('frontEnd.register.register') }}">Register</a>
+                <a class="btn-book-a-table btn-sm log" href="{{ route('frontEnd.login.login') }}">Login</a>
+            </div>
+        </div>
     </div>
-  </header>
+</header>
